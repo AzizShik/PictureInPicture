814d0195705a8e49454cc8722b11cf407199bc9e
Abdyl-Aziz <azizshihabov@mail.ru>
2022-11-09 20:08:04 +0300

    feat: add functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-window.addEventListener('load', () => {});
+window.addEventListener('load', () => {
+	const videoEl = document.querySelector('#video');
+	const btnEl = document.querySelector('#btn');
+
+	async function selectMediaStream() {
+		try {
+			const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+			videoEl.srcObject = mediaStream;
+			videoEl.onloadedmetadata = () => {
+				videoEl.play();
+			};
+		} catch (e) {
+			console.error('Error', e);
+		}
+	}
+
+	btnEl.addEventListener('click', async () => {
+		btnEl.disabled = true;
+		await videoEl.requestPictureInPicture();
+		btnEl.disabled = false;
+	});
+
+	selectMediaStream();
+});
